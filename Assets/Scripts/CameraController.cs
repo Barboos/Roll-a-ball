a77753b9850e54040b7e7e6c68aa46bcdbc6d024
Barboos <33928663+Acraiu@users.noreply.github.com>
2019-10-18 03:05:38 +0300

    Add full game

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;//distanta de la camera la obiect
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is called once per frame after Update
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rigidBody;
+
+    private int score;
+    public Text scoreText;
+    public Text winText;
+
+
+
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        score = 0;
+        SetScoreText();
+        SetWinText();
+	}
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rigidBody.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            SetScoreText();
+            if (score == 12)
+            {
+                winText.enabled = true;
+            }
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    void SetWinText()
+    {
+        winText.text = "You win!";
+        winText.enabled = false;
+    }
+}
